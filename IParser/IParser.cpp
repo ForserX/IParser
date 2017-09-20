cc54f49a47ae6135a8301e8df91436b52517c693
sv3nk <sv3nk@yandex.ru>
2017-09-20 21:15:27 +0300

    * ADD: Added IParser * ADD: Added Debug project

---
+++ IParser/IParser.cpp
@@ -0,0 +1,16 @@
+// IParser.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include "IParserSystem.hpp"
+#include <Windows.h>
+#include <iostream>
+
+int main()
+{
+	IFS::config ini = IFS::config(std::string("..//test.ltx"));
+	std::cout << ini.get_value("sect", "key") << std::endl;
+	system("pause");
+    return 0;
+}
+
